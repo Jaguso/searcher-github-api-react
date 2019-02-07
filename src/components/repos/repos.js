30f5f254df1bcc37ce99abe4242b6cb33846161e
Jaguso <jagutzo@gmail.com>
2019-02-07 16:38:32 -0600

    made component for repositories list

---
+++ src/components/repos/repos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Repos extends Component {
+  render() {
+    return(
+      <div>
+        <h3>These are the repositories</h3>
+      </div>
+    );
+  }
+}
+
+export default Repos;
