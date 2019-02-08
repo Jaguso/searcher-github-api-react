51ffeefb34941d854666be275c487120cf5207c5
Jaguso <jagutzo@gmail.com>
2019-02-08 00:01:02 -0600

    added basic search for users

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -6,7 +6,8 @@ class Users extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      search: ""
     }
   }
 
@@ -19,14 +20,39 @@ class Users extends Component {
     })
   }
 
+  onChangeInput = (event) => {
+    const {name, value} = event.target
+
+    console.log("evento: ", event);
+    console.log("valores: ", name, value);
+    this.setState({[name]: value})
+  }
+
   render() {
+
+    let names = this.state.users.map(user => user.login)
+    
+    let filteredUsers = names.filter(
+      (name) => {
+        return name.indexOf(this.state.search) !== -1;
+      }
+    );
+
     return(
       <div>
         <h3>Users</h3>
+          <label>
+            Buscar un usuario
+            <input 
+              type="text"
+              name="search"
+              onChange={this.onChangeInput}
+            />
+          </label>
           <ul>
-            {this.state.users.map(user => (
+            {filteredUsers.map(user => (
               <li>
-                {user.login}
+                {user}
               </li>
             ))}
           </ul>
@@ -38,4 +64,4 @@ class Users extends Component {
   }
 }
 
-export default Users
+export default Users;
--- src/services/index.js
+++ src/services/index.js
@@ -4,7 +4,7 @@ const API_URL = "https://api.github.com";
 
 const getUsers = () => axios.get(`${API_URL}/users`);
 
-const getRepos = () => axios.get(`${API_URL}/orgs/octokit/repos`)
+const getRepos = () => axios.get(`${API_URL}/orgs/octokit/repos`);
 
 
 export { getUsers, getRepos };
